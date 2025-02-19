f8f8799216b97cb45356571594bb89703aa13ff7
softedge12 <softedgesoftwares12@gmail.com>
2025-02-19 11:03:24 +0530

    Add files via upload

---
+++ gsquizsubjects/history/part1/questions - Copy.js
@@ -0,0 +1,27 @@
+
+let jsq_questions = [
+	{"id":1,"question":"ram kaun hain","options": {"a":"manav","b":"devta","c":"insan","d":"bhagwan"},"answer": "d"},
+	
+{
+		"id": 2,
+		"question": "krishna kaun hain",
+		"options": {
+			"a": "bhagwan",
+			"b": "manav",
+			"c": "devta",
+			"d": "insan"
+		},
+		"answer": "a"
+	},
+	{
+		"id": 3,
+		"question": "hanuman kaun hain",
+		"options": {
+			"a": "bhagwan",
+			"b": "manav",
+			"c": "devta",
+			"d": "insan"
+		},
+		"answer": "d"
+	}
+];
